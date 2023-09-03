407a28d087ca8c710654afec0f41975de6ef879e
Vũ Thành Công <104372944+vuthanhcong02@users.noreply.github.com>
2023-09-03 11:39:24 +0700

    fix style header and course

--- app/src/components/CartCourse/CartCourse.js
+++ app/src/components/CartCourse/CartCourse.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './CartCourse.scss'
+import { Link } from "react-router-dom";
 export default function CartCourse() {
   return (
     <>
@@ -7,7 +8,7 @@ export default function CartCourse() {
             <span className="tag">TLU</span>
             <img className="course-img" src="https://picsum.photos/200/300" alt=""/>
            <div className="course-info">
-                <span className="course-name">Java Web</span>
+                <Link to="#"><span className="course-name">Java Web</span></Link>
                 <p className="course-desc">Khoá học lập trình web từ cơ bản đến nâng cao</p>
             </div>
         </div>
